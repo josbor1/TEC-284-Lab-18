6e095474297ee30d37ff4f1d1b3537deab10a024
josbor1 <123982835+josbor1@users.noreply.github.com>
2025-04-01 13:47:05 -0500

    Update Lab18Start.py
    Simplified code. LEDs are controlled individually instead of using LEDBoard method.

--- Lab18Start.py
+++ Lab18Start.py
@@ -1,16 +1,22 @@
 import time
 import random
-from gpiozero import LEDBoard, Button
+from gpiozero import LED, Button
 
 # Define GPIO pins
-LEDnumber = LEDBoard(23, 4, 25, 10, 17, 8, 22, pwm=False)  # Adjust pin numbers if needed
+led0 = LED(23)
+led1 = LED(4)
+led2 = LED(25)
+led3 = LED(10)
+led4 = LED(17)
+led5 = LED(8)
+led6 = LED(22)  # Adjust pin numbers if needed
 pushButton = Button(7)  # Adjust pin number if needed
 
 def main():
     try:
         while True:
-            number = generateNumber()
-            displayNumber(number)
+            number = generateNumber() #generate a random number
+            displayNumber(number) #dissplay that number
     except KeyboardInterrupt:
         print("\nExiting program.")
 
@@ -21,7 +27,7 @@ def generateNumber():  # Wait for a push, return random die value
     pushButton.wait_for_press()  # Wait for button press
     time.sleep(0.030)  # Debounce delay
     
-    while pushButton.is_pressed:
+    while pushButton.is_pressed: #Cycles through 1-6
         throw += 1
         if throw > 6:  # Wrap round the number
             throw = 1
@@ -30,15 +36,31 @@ def generateNumber():  # Wait for a push, return random die value
     return throw
 
 def displayNumber(number):
-    # Light up LEDs corresponding to the dice number
-    for i, led in enumerate(LEDnumber):
-        if i < number:
-            led.on()
-        else:
-            led.off()
-            
-    time.sleep(1)  # Keep the LEDs on for 1 second
-    LEDnumber.off()  # Turn off all LEDs
+
+    ledsOff()
+    
+    #This is an example of how to turn on LEDs for a specific number
+    if number == 5:
+        led0.on()
+        led1.on()
+        led3.on()
+        led5.on()
+        led6.on()
+        
+    time.sleep(1)
+    
+    ledsOff()
+
+def ledsOff():
+    #Turn off all LEDs.
+    led0.off()
+    led1.off()
+    led2.off()
+    led3.off()
+    led4.off()
+    led5.off()
+    led6.off()
+    
 
 if __name__ == "__main__":
     main()
